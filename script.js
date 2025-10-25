a0b1b1634ea1590dd4880c83ed41c0c9fc0b854c
mathieuwillett <143769896+mathieuwillett@users.noreply.github.com>
2025-10-24 20:15:27 -0400

    scroll trigger gsap fait pour toutes les pages

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const appli = Vue.createApp({
         this.message = "Vue a été chargé et montée au DOM (mounted) !";
 
         fetch("./projects.json")
-            .then(data => data.json()) 
-            .then(result => { 
+            .then(data => data.json())
+            .then(result => {
                 console.log(result);
                 this.projetsArr = result
                 this.selectedProject = this.projetsArr[0]
@@ -33,10 +33,131 @@ const appli = Vue.createApp({
             } else if (this.index <= -1) {
                 this.index = this.projetsArr.length - 1
             }
-        
+
             this.selectedProject = this.projetsArr[this.index]
         }
     }
 });
 
 const vm = appli.mount('.appli-vue');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from("header", {
+        opacity: 0,
+        duration: 1,
+    });
+
+    gsap.from(".section-carrousel", {
+        opacity: 0,
+        duration: 1,
+    });
+
+    gsap.from(".section-hero", {
+        opacity: 0,
+        duration: 1,
+    });
+
+    gsap.from(".section-competences", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-competences",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-projets", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-projets",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+     gsap.from(".section-propos", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-propos",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from("footer", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "footer",
+            start: "top 100%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-description", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-description",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-collaborateurs", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-collaborateurs",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-galerie", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-galerie",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-oeuvre", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-oeuvre",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+
+    gsap.from(".section-processus", {
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: ".section-processus",
+            start: "top 80%",
+            end: "bottom 60%",
+            toggleActions: "play none none reverse",
+        }
+    });
+});
+
+
